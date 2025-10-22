f2f32d3db2ae916575a9f42f1a6f73e90abadbb1
harshith <harshithr2004@gmail.com>
2025-10-22 20:06:09 +0530

    string parser, parsing the Request Line

--- internal/request/request.go
+++ internal/request/request.go
@@ -17,10 +17,6 @@ type Request struct {
 	RequestLine RequestLine
 }
 
-func (r *RequestLine) ValidHttp() bool {
-	return r.HttpVersion == "HTTP/1.1"
-}
-
 var ERROR_MALFORMED_REQUEST_LINE = fmt.Errorf("malformed request-line")
 var ERROR_UNSUPPORTED_HTTP_VERSION = fmt.Errorf("error unsupported http version")
 
@@ -33,19 +29,22 @@ func parseRequestLine(b string) (*RequestLine, string, error) {
 	}
 	startLine := b[:idx]
 	restOfMessage := b[idx+len(SEPERATOR):]
+
 	parts := strings.Split(startLine, " ")
 	if len(parts) != 3 {
 		return nil, restOfMessage, ERROR_MALFORMED_REQUEST_LINE
 	}
+	httpParts := strings.Split(parts[2], "/")
+	if len(httpParts) != 2 || httpParts[0] != "HTTP" || httpParts[1] != "1.1" {
+		return nil, restOfMessage, ERROR_MALFORMED_REQUEST_LINE
+	}
+
 	rl := &RequestLine{
 		Method:        parts[0],
 		RequestTarget: parts[1],
-		HttpVersion:   parts[2],
+		HttpVersion:   httpParts[1],
 	}
 
-	if !rl.ValidHttp() {
-		return nil, restOfMessage, ERROR_UNSUPPORTED_HTTP_VERSION
-	}
 	return rl, restOfMessage, nil
 }
 
@@ -62,6 +61,9 @@ func RequestFromReader(reader io.Reader) (*Request, error) {
 	str := string(data)
 
 	rl, _, err := parseRequestLine(str)
+	if err != nil {
+		return nil, err
+	}
 
 	return &Request{
 		RequestLine: *rl,
